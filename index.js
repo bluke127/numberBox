faf54ffc440fa238cd093f5617ea6f99c8cef6df
blukoo <dnehdrkd123@naver.com>
2021-10-24 13:17:02 +0900

    [fix] 전역 변수 삭제

--- index.js
+++ index.js
@@ -1,36 +1,19 @@
-//행과 열의 숫자를 담아줄 배열
-let rowNCol = [];
+//row 와 col은 createNumberBox나 checkNum 등 많은 곳엣 사용
 //행
 let row = null;
 //열
 let col = null;
-//row와 col 중에 큰 수와 작은 수
-let bigger;
-let small;
+//정답이 포함된 숫자들이 있는 전역변수
 //정답의 숫자들을 하나하나를 배열로 담음
 let num = { answer: [], question: [] };
-//num.ques를 랜덤으로 담아줄, 또 순차적으로 li에 뿌려줄 배열
-let boxValue = [];
-//사용자가 클릭한 숫자의 배열
-let userAnswer = [];
-//사용자가 몇 번 클릭했는지 세어주며, 이 변수로 userAnswer와 answer의 값을 비교 예정
-let answerIndex = 0;
+//상황에 따라 팝업을 닫고 열어주는 전역변수
 //팝업을 닫아줄 변수
 let isClosePop = false;
-//비동기를 통해 답이 몇번째 상자에 들어갔는지 확인해주는 객체변수
-let setShowingAnswer = { answerOrder: [], boxValueOrder: [] };
 //클릭시 화살표 모양 바뀌는 flag
 let isAnswer = false;
-//결과 img경로
-let resultSrc = "";
-//결과 message
-let resultMessage = "";
 //각 값이 모두 true때(입력한 값이 정상적일때) 게임 시작
-let isRow = false;
-let isCol = false;
-let passRowNCol = [isRow, isCol];
-//에러 메세지를 저장해서, 오류 생기면 alert(errorMessage)
-let errorMessage = "";
+//0번째는 row의 pass 조건, 1번째는 col의 pass 조건
+let passRowNCol = [false, false];
 //자주 쓰는 클래스네임
 let BOX_ELEMENT = document.getElementsByClassName("boxElement");
 
@@ -112,8 +95,8 @@ async function insertNumber() {
     return;
   }
   //상자의 가로 세로 갯수
-  rowNCol.push(row);
-  rowNCol.push(col);
+  // rowNCol.push(row);
+  // rowNCol.push(col);
   let answerLength;
   //큰 숫자 만큼 정답의 숫자 갯수가 정해짐!
   row > col ? (answerLength = row) : (answerLength = col);
@@ -130,6 +113,11 @@ async function insertNumber() {
    맨처음에 보여질 정답은 3번째의 정답 값임으로 순서대로 정답을 입력해야하는 이 게임에 적합하지 않음
    아래에 수정사항이 나옴,, 좀 복잡하게 푼 감도,,,
   */
+
+  //num.ques를 랜덤으로 담아줄, 또 순차적으로 li에 뿌려줄 배열
+  let boxValue = [];
+  //비동기를 통해 답이 몇번째 상자에 들어갔는지 확인해주는 객체변수
+  let setShowingAnswer = { answerOrder: [], boxValueOrder: [] };
   for (let ii = 0; ii < row * col; ii++) {
     if (indexForStore[ii] < num.answer.length) {
       setShowingAnswer.boxValueOrder.push(ii);
@@ -142,7 +130,7 @@ async function insertNumber() {
   //위에 [3,1,2,0]의 공간값 저장배열이라면 [3(0이 3번째),1(1이 1번째),2(2가 2번째),0(3이 0번째)]
   //이 메서드를 통해 answerOrder를 정리
   returnIndex(setShowingAnswer.answerOrder);
-  createNumberBox();
+  createNumberBox(boxValue);
   setArrow();
   changeText("textPop", "정답은???");
   changeText("answerText", `${num.answer}`);
@@ -205,6 +193,8 @@ function returnOrigin(i) {
 
 //처음에 submit 할때 boxRowCol 입력한 값을 넣게 했으나 정규식 확인을 효율적으로 할 수 있어 keyup 에 넣음
 function checkNum(index) {
+  //에러 메세지를 저장해서, 오류 생기면 alert(errorMessage)
+  let errorMessage = "";
   let number = document.querySelectorAll(".setNum");
   //백스페이스와 새로고침 누를 때는 이 메서드 실행하지 말아주세요
   if (event.keyCode == 116 || event.keyCode === 8) {
@@ -218,7 +208,7 @@ function checkNum(index) {
     return;
   }
   //입력한 값을 rowNCol에
-  rowNCol[index] = number[index].value;
+  // rowNCol[index] = number[index].value;
   //박스 게임 생성 배열 true
   passRowNCol[index] = true;
   index === 0 ? (row = number[index].value) : (col = number[index].value);
@@ -252,15 +242,15 @@ function regexNumber(value) {
   return value;
 }
 
-function createNumberBox() {
-  var li = document.getElementById("box").getElementsByTagName("li");
-  for (var i = 0; i < boxValue.length; i++) {
-    let element = `<li class='boxElement'>${boxValue[i]}</li>`;
+function createNumberBox(array) {
+  let li = document.getElementById("box").getElementsByTagName("li");
+  for (var i = 0; i < array.length; i++) {
+    let element = `<li class='boxElement'>${array[i]}</li>`;
     if (BOX_ELEMENT.length <= 0) {
       document.getElementById("box").innerHTML = element;
     } else {
       var e = document.createElement("li");
-      var t = document.createTextNode(`${boxValue[i]}`);
+      var t = document.createTextNode(`${array[i]}`);
       e.appendChild(t);
       document.getElementById("box").appendChild(e);
     }
@@ -277,56 +267,58 @@ function createNumberBox() {
   for (var i = 0; i < li.length; i++) {
     BOX_ELEMENT[i].style.width = 500 / row + "px";
     BOX_ELEMENT[i].style.height = 500 / row + "px";
-    BOX_ELEMENT[i].style.fontSize = 60 / (small / 2) + "px";
+    BOX_ELEMENT[i].style.fontSize = "1em";
     BOX_ELEMENT[i].style.lineHeight = 500 / row + "px";
   }
 }
-//사용자가 클릭한 숫자를 userAnswer배열에 넣는 메서드
-function getTryingAnswer(i) {
-  userAnswer.push(boxValue[i]);
-  var li = document.getElementById("collection");
-  var resultText = document.getElementById("resultText");
-  //클로저변수를 이용해서 클릭할때마다 클로저의 변수는 ++, 이전에 answer와 입력한 값을 비교함
-  //만일 사용자가 틀린 답을 입력할때,
-  if (num.answer[answerIndex] !== userAnswer[answerIndex]) {
-    inNOutArea("result", 0);
-    alert("진실의 방으로");
-    resultSrc = "./src/assets/img/punch.png";
-    resultMessage = "한 판 더 하실?";
-    //변수 초기화
-    userAnswer = [];
-    answerIndex = 0;
-    li.innerText = "";
-    //맞게 입력하면 closure ++할 메서드 실행! 선택한 숫자에는 입력한 숫자게 계속 들어감
-  } else {
-    var text = [...userAnswer];
-    li.innerText = text;
-    tryAnswer(answerIndex);
-  }
-  //userAnswer와 answer의 길이가 같고, 마지막 값까지 같다? 그럼 통과가 됨, 한번이라도 틀리면 길이가 틀려지고, 마지막값도 같다면 완벽히 일치!
-  if (
-    num.answer[num.answer.length] === userAnswer[userAnswer.length] &&
-    userAnswer.length === num.answer.length
-  ) {
-    inNOutArea("result", 0);
-    resultSrc = "./src/assets/img/thumbsUp.png";
-    resultMessage = "잇츠 굿~~~";
-    //변수 초기화
-    userAnswer = [];
-    answerIndex = 0;
-    li.innerText = "";
-  }
-  document.getElementsByTagName("img")[0].setAttribute("src", `${resultSrc}`);
-  resultText.innerText = resultMessage;
-}
-const tryAnswer = function (c) {
-  answerIndex = c;
-  function updateCloser() {
-    answerIndex++;
-  }
-  return updateCloser();
-};
-
+const getTryingAnswer = (function () {
+  //사용자가 몇 번 클릭했는지 세어주며, 이 변수로 userAnswer와 answer의 값을 비교 예정
+  let answerIndex = 0;
+  let userAnswer = [];
+  //사용자가 클릭한 숫자를 userAnswer배열에 넣는 메서드
+  return function innerGetTryingAnswer(i) {
+    console.log(answerIndex);
+    //결과 img경로
+    let resultSrc = "";
+    //결과 message
+    let resultMessage = "";
+    userAnswer.push(event.target.innerText);
+    var li = document.getElementById("collection");
+    var resultText = document.getElementById("resultText");
+    //클로저변수를 이용해서 클릭할때마다 클로저의 변수는 ++, 이전에 answer와 입력한 값을 비교함
+    //만일 사용자가 틀린 답을 입력할때,
+    if (num.answer[answerIndex] != userAnswer[answerIndex]) {
+      inNOutArea("result", 0);
+      alert("진실의 방으로");
+      resultSrc = "./src/assets/img/punch.png";
+      resultMessage = "한 판 더 하실?";
+      //변수 초기화
+      userAnswer = [];
+      answerIndex = 0;
+      li.innerText = "";
+      //맞게 입력하면 closure ++할 메서드 실행! 선택한 숫자에는 입력한 숫자게 계속 들어감
+      //userAnswer와 answer의 길이가 같고, 마지막 값까지 같다? 그럼 통과가 됨, 한번이라도 틀리면 길이가 틀려지고, 마지막값도 같다면 완벽히 일치!
+    } else if (
+      num.answer[num.answer.length - 1] == userAnswer[userAnswer.length - 1] &&
+      userAnswer.length === num.answer.length
+    ) {
+      console.log(num.answer, userAnswer);
+      inNOutArea("result", 0);
+      resultSrc = "./src/assets/img/thumbsUp.png";
+      resultMessage = "잇츠 굿~~~";
+      //변수 초기화
+      userAnswer = [];
+      answerIndex = 0;
+      li.innerText = "";
+    } else {
+      var text = [...userAnswer];
+      li.innerText = text;
+      answerIndex++;
+    }
+    document.getElementsByTagName("img")[0].setAttribute("src", `${resultSrc}`);
+    resultText.innerText = resultMessage;
+  };
+})();
 /**answer */
 function setArrow() {
   if (isAnswer === true) {
@@ -350,19 +342,10 @@ function closePopup(flag) {
 }
 function retry() {
   num = { answer: [], question: [] };
-  rowNCol = [];
-  boxValue = [];
-  userAnswer = [];
-  answerIndex = 0;
+  // rowNCol = [];
+  // boxValue = [];
   isClosePop = false;
   isAnswer = false;
-  setShowingAnswer = { answerOrder: [], boxValueOrder: [] };
-  resultSrc = "";
-  resultMessage = "";
-  errorMessage = "";
-  let isRow = false;
-  let isCol = false;
-  let passRowNCol = [isRow, isCol];
   //에러 메세지를 저장해서, 오류 생기면 alert(errorMessage)
   inNOutArea("popUpWrap", 0);
   changeText("textPop", "상자의 열과 행을 입력해주세요");
